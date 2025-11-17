8166232c07ad7e0dc16899734244190f9342b227
cnaegele <cnaegele@lausanne.ch>
2025-11-17 16:23:43 +0100

    possibilite passage parametre status

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-      <AnnonceTravauxListe :ssServer="ssServer" :demandestatus="40"></AnnonceTravauxListe>
+  <AnnonceTravauxListe :ssServer="ssServer" :demandestatus="demandesStatus"></AnnonceTravauxListe>
 </template>
 
 <script setup lang="ts">
+import { stringToPositiveInteger } from '@/jaxformsOpcAnnonceTravauxImport.ts'
 import { ref } from 'vue'
-  const ssServer = ref<string>('')
+const ssServer = ref<string>('')
 if (import.meta.env.DEV) {
   ssServer.value = 'https://mygolux.lausanne.ch'
+}
+
+//Possibilité de passer le status en paramètre
+const demandesStatus = ref<string>('40') //status traité
+const urlParams = new URLSearchParams(window.location.search)
+const prmDemandesStatus = urlParams.get('status')
+if (prmDemandesStatus) {
+  if (/^\d{1,3}$/.test(prmDemandesStatus)) {
+    demandesStatus.value = prmDemandesStatus
+  }
 }
 </script>
